62e11cf2b1f92b37b6f562706211ce0ab9d869e2
김태욱 <2018008186@hanyang.ac.kr>
2022-04-04 16:36:54 +0900

    add PostGet

--- post/views.py
+++ post/views.py
@@ -3,6 +3,21 @@ from rest_framework import viewsets
 from post.models import Post
 from post.serializers import PostSerializer
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class PostListAPIView(APIView):
+     def get(self, request):
+         serializer = PostSerializer(Post.objects.all(), many=True)
+         return Response(serializer.data)
+
+from django.shortcuts import get_object_or_404
+
+class PostDetailAPIView(APIView):
+    def get_object(self, pk):
+    return get_object_or_404(Post, pk=pk)
+
+    def get(self, request, pk, format=None):
+    post = self.get_object(pk)
+    serializer = PostSerializer(post)
+    return Response(serializer.data)
