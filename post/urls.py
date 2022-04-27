2d80fc1d9f3112cb9b8b2c7cb12d490f036b4ccf
김주력 <ryan9423@hanyang.ac.kr>
2022-04-27 19:49:13 +0900

    add slash at the end

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,5 @@ app_name = 'post'
 
 urlpatterns = [
     path('', views.PostListAPIView.as_view(), name='all'),
-    path('<int:pk>', views.PostDetailAPIView.as_view(), name='detail'),
+    path('<int:pk>/', views.PostDetailAPIView.as_view(), name='detail'),
 ]
