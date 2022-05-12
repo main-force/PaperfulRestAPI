6eb9064bec500ff52946ca78dcc2be75f777f78d
김주력 <ryan9423@hanyang.ac.kr>
2022-05-12 22:44:36 +0900

    add intermediate admin model

--- comment/admin.py
+++ comment/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from comment.models import Comment
+from comment.models import Comment, Attention, WriterMention
 
 
 admin.site.register(Comment)
+admin.site.register(Attention)
+admin.site.register(WriterMention)
--- post/admin.py
+++ post/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from post.models import Post
-
+from post.models import Post, PostTag, Attention, Tag
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     readonly_fields = ('create_at', 'update_at')
     search_fields = ('title', 'user__email')
+
+
+admin.site.register(PostTag)
+admin.site.register(Attention)
+admin.site.register(Tag)
