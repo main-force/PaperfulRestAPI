523abdbf3fc87867cf0488947ad5f036a4ed0a26
김주력 <ryan9423@hanyang.ac.kr>
2022-05-12 22:38:05 +0900

    change admin url

--- PaperfulRestAPI/urls.py
+++ PaperfulRestAPI/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('posts', include('post.urls')),
     path('auth', obtain_auth_token, name='obtain-auth-token'),
     path('logout', Logout.as_view(), name='logout'),
