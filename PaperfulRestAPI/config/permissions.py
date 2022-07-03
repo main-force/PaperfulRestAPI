d4ad1aa31debc76c84d3998b161b8c2e78e0b2f5
김주력 <ryan9423@hanyang.ac.kr>
2022-07-04 01:24:27 +0900

    add status type in permission'

--- PaperfulRestAPI/config/permissions.py
+++ PaperfulRestAPI/config/permissions.py
@@ -46,36 +46,40 @@ class BaseIsOwnerOrReadOnlyWithQueryParams(BasePermission):
     query_params = {}
 
     def has_object_permission(self, request, view, obj):
+        has_permission = True
         for key in [ele for ele in self.query_params.keys() if(ele in request.query_params.keys())]:
-            if self.query_params[key] == request.query_params[key]:
+            if request.query_params[key] in self.query_params[key]:
                 if request.user.is_authenticated:
                     if request.user.is_staff:
-                        return True
+                        pass
                     elif hasattr(obj, 'user'):
-                        return obj.user == request.user
+                        has_permission = has_permission and (obj.user == request.user)
                     elif hasattr(obj, 'writer'):
-                        return obj.writer.user == request.user
+                        has_permission = has_permission and (obj.writer.user == request.user)
                     elif obj.__class__ == get_user_model():
-                        return obj.id == request.user.id
-                    return False
+                        has_permission = has_permission and (obj.id == request.user.id)
+                    else:
+                        return False
                 else:
                     return False
         if request.method in SAFE_METHODS:
-            return True
+            pass
         else:
             if request.user.is_authenticated:
                 if request.user.is_staff:
-                    return True
+                    pass
                 elif hasattr(obj, 'user'):
-                    return obj.user == request.user
+                    has_permission = has_permission and (obj.user == request.user)
                 elif hasattr(obj, 'writer'):
-                    return obj.writer.user == request.user
+                    has_permission = has_permission and (obj.writer.user == request.user)
                 elif obj.__class__ == get_user_model():
-                    return obj.id == request.user.id
-                return False
+                    has_permission = has_permission and (obj.id == request.user.id)
+                else:
+                    return False
             else:
                 return False
 
+        return has_permission
 
 class IsOwnerOrReadOnlyWithPostStatus(BaseIsOwnerOrReadOnlyWithQueryParams):
-    query_params = {'status': 'T'}
+    query_params = {'status': ['T', 'P', 'B']}
